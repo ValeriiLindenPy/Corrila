582a5e903e02fc3b1737d4b14a954aa97fd06d8e
ElenaLindenPy <119326180+ElenaLindenPy@users.noreply.github.com>
2023-05-22 10:11:37 +0400

    main commit

---
+++ core/models.py
@@ -0,0 +1,58 @@
+from ckeditor.fields import RichTextField
+from django.contrib.auth.models import User
+from django.db import models
+from django.urls import reverse
+from django.utils.text import slugify
+
+# Create your models here.
+
+
+class Article(models.Model):
+    title = models.CharField(max_length=250, blank=False)
+    preview_text = models.TextField(
+        blank=False, default='Somthing intresting inside the article. Just read it!')
+    text = RichTextField(blank=False, null=True,
+                         default='Write somthing about statisics or correlation')
+    author = models.ForeignKey(User, on_delete=models.PROTECT)
+    pubplication_date = models.DateTimeField(auto_now_add=True)
+    slug = models.SlugField(max_length=255, unique=True,
+                            db_index=True, verbose_name='URL')
+    moderated = models.BooleanField(default=False)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('article', kwargs={'article_slug': self.slug})
+
+
+class Report(models.Model):
+    title = models.CharField(max_length=250, blank=True)
+    low_correlaton_result = models.TextField()
+    high_correlaton_result = models.TextField()
+    correlatons_range = models.CharField(max_length=250, blank=True)
+    correlaton_type = models.CharField(max_length=250)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, blank=True, null=True)
+    pubplication_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Report of user {self.author} named "{self.title}" - {self.pubplication_date}'
+
+    def get_absolute_url(self):
+        return reverse('report', kwargs={'report_id': self.pk})
+
+
+class Feedback(models.Model):
+    first_name = models.CharField(max_length=250)
+    last_name = models.CharField(max_length=250)
+    email = models.EmailField()
+    message = models.TextField(max_length=1000)
+    sending_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Feedback from {self.email} - {self.sending_date}'
---
+++ core/views.py
@@ -0,0 +1,175 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, redirect, render
+from django.urls import reverse_lazy
+from django.utils.datastructures import MultiValueDictKeyError
+from django.views import View
+from django.views.generic import ListView, TemplateView
+from django.views.generic.base import TemplateView
+from django.views.generic.edit import FormView
+
+from .forms import CreateArticleForm, FeedbackForm
+from .models import *
+from .utils import CorrelationTools
+
+# Create your views here.
+
+
+def previous_page(request):
+    # Get the previous page URL from the request headers
+    previous_url = request.META.get("HTTP_REFERER")
+
+    # Redirect to the previous URL
+    return redirect(previous_url)
+
+
+class ShowHome(View):
+    articles = Article.objects.all()[:3]
+    content = {"title": "Corrila - Free Online Data Correlation", "description_data":
+               "Corrila provides free online tools for data correlation. Choose the type of correlation coefficient: Pearson, Spearman or Kendall and get the results without having to download additional software.", "articles": articles}
+    template = "index.html"
+
+    def get(self, request):
+        return render(request, self.template, self.content)
+
+
+class ShowAbout(TemplateView):
+    template_name = "about.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["title"] = "About us - Corrila"
+        return context
+
+
+class ShowArticles(ListView):
+    paginate_by = 2
+    model = Article
+    context_object_name = "articles"
+    template_name = "articles.html"
+    extra_context = {"title": "Articles - Corrila"}
+
+
+class ShowArticlePage(View):
+    template = "articles-page.html"
+
+    def get(self, request, article_slug):
+        article = get_object_or_404(Article, slug=article_slug)
+        content = {"title": f"{article.title} - Corrila", "article": article}
+
+        return render(request, self.template, content)
+
+
+class ShowCorrelation(View):
+    content = {"title": "Correlate data"}
+    template = "correlate.html"
+
+    def get(self, request):
+        return render(request, self.template, self.content)
+
+    def post(self, request):
+        try:
+            file = request.FILES["excel_file"]
+        except MultiValueDictKeyError:
+            # Redirect the user to the desired URL or display an error message
+            return redirect('error')
+
+        high_choice = False
+        low_choice = False
+
+        if request.POST.get("pearson", False) == "on":
+            correlaton_type_chosen = "pearson"
+        if request.POST.get("spearman", False) == "on":
+            correlaton_type_chosen = "spearman"
+        if request.POST.get("kendall", False) == "on":
+            correlaton_type_chosen = "kendall"
+
+        if request.POST.get("high", False) == "on":
+            high_choice = True
+        if request.POST.get("low", False) == "on":
+            low_choice = True
+
+        correlator = CorrelationTools()
+        correlator.filter_low_high_corr(
+            file, method_chosen=correlaton_type_chosen)
+
+        if request.user.is_authenticated:
+            title = request.POST.get("title", False)
+            if low_choice:
+                low = correlator.get_low_corr()
+            else:
+                low = "Low correlation range has not been chosen"
+
+            if high_choice:
+                high = correlator.get_high_corr()
+            else:
+                high = "High correlation range has not been chosen"
+
+            Report.objects.create(
+                title=title,
+                correlaton_type=correlaton_type_chosen,
+                low_correlaton_result=low,
+                high_correlaton_result=high,
+                author=request.user,
+            )
+            content = {
+                "title": title,
+                "correlaton_type": correlaton_type_chosen,
+                "low_correlaton_result": low,
+                "high_correlaton_result": high,
+                "author": request.user.username,
+            }
+            return render(request, "report.html", content)
+        else:
+            if low_choice:
+                low = correlator.get_low_corr()
+            else:
+                low = "Low correlation range has not been chosen"
+
+            if high_choice:
+                high = correlator.get_high_corr()
+            else:
+                high = "High correlation range has not been chosen"
+
+            content = {
+                "correlaton_type": correlaton_type_chosen,
+                "low_correlaton_result": low,
+                "high_correlaton_result": high,
+            }
+            return render(request, "report.html", content)
+
+
+class FeedbackFormView(FormView):
+    template_name = 'submit-feedback.html'
+    form_class = FeedbackForm
+    success_url = reverse_lazy('success')
+
+    def form_valid(self, form):
+        form.save()
+        return super().form_valid(form)
+
+
+class FeedSuccessbackFormView(TemplateView):
+    template_name = "success-feedback.html"
+
+
+class ErrorView(TemplateView):
+    template_name = "error.html"
+
+
+class CreateArticleView(View, LoginRequiredMixin):
+    content = {"title": "Correlate data"}
+    template = "create-article.html"
+
+    def get(self, request):
+        form = CreateArticleForm()  # Create an instance of the form
+        return render(request, self.template, {'form': form, **self.content})
+
+    def post(self, request):
+        form = CreateArticleForm(request.POST)
+        if form.is_valid():
+            article = form.save(commit=False)
+            article.author = request.user
+            article.save()
+            return redirect('article', article_slug=article.slug)
+        return render(request, self.template, {'form': form})
