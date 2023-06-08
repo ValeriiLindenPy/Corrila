8b46aa579c42f93ddcc09c14fba114ed089bfeb8
Valerii <valeriilinden@gmail.com>
2023-06-08 17:27:14 +0400

    new 8

--- corrila/celery.py
+++ corrila/celery.py
@@ -9,4 +9,9 @@ app = Celery('corrila')
 
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
+app.conf.broker_transport_options = {
+    'visibility_timeout': 3600,
+    'ssl_cert_reqs': 'CERT_REQUIRED'
+}
+
 app.autodiscover_tasks()
